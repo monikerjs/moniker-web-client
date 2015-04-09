27aa7a2e889172ed9bcb8ec69c4fa4a323c8298a
mattstyles <matt@veryfizzyjelly.com>
2015-04-09 22:42:11 +0100

    #update test full suite integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,25 @@
 
 import React from 'react';
+import request from 'superagent';
+
 import dispatcher from './dispatchers/appDispatcher';
 
+
 console.log( 'its alive' );
+
+var btn = document.querySelector( '#get' );
+btn.addEventListener( 'click', event => {
+    request
+        .get( '/api/categories' )
+        .end( ( err, res ) => {
+            if ( err ) {
+                return console.error( err );
+            }
+
+            if ( !res.ok ) {
+                return console.log( 'Something funky going on with result. ', res );
+            }
+
+            console.log( JSON.stringify( res.body ) );
+        });
+});
